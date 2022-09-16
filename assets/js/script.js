461a97932744f93c898e1a67d9985edcd70d7dd8
NicoHndzHc <nico.hndz.s@gmail.com>
2022-09-16 13:55:41 -0300

    Se completa sección gaming y se agrega Js a botones.

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+function compraAlert(){
+  alert("Gracias por su compra")
+}
+
+let botonComprar = document.getElementById("boton-comprar");
+botonComprar.addEventListener("click", compraAlert);
+
+let botonComprar2 = document.getElementById("boton-comprar2");
+botonComprar2.addEventListener("click", compraAlert);
+
+let botonComprar3 = document.getElementById("boton-comprar3");
+botonComprar3.addEventListener("click", compraAlert);
+
+let botonComprar4 = document.getElementById("boton-comprar4");
+botonComprar4.addEventListener("click", compraAlert);
+
+let botonComprar5 = document.getElementById("boton-comprar5");
+botonComprar5.addEventListener("click", compraAlert);
+
+let botonComprar6 = document.getElementById("boton-comprar6");
+botonComprar6.addEventListener("click", compraAlert);
